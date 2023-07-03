ac22a41e7dc35e7cdfb11f1a148ef6dee29a5709
Boris Antonijev <antonijevboris@gmail.com>
2023-07-03 22:03:16 +0200

    tailwind initialize

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function App() {
   return (
     <div>
-      <h1>BORIS</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
