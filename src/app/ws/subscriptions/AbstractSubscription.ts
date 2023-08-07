f48276bf1b90c8fa669396edf0022c612ef19525
Boris Antonijev <antonijevboris@gmail.com>
2023-08-07 22:32:44 +0200

    getters and setters for abstract subscription

--- src/app/ws/subscriptions/AbstractSubscription.ts
+++ src/app/ws/subscriptions/AbstractSubscription.ts
@@ -6,4 +6,20 @@ export default abstract class AbstractSubscription {
     this._topic = topic;
     this._callback = callback;
   }
+
+  public get topic(): string {
+    return this._topic;
+  }
+
+  public set topic(value: string) {
+    this._topic = value;
+  }
+
+  public get callback(): (message: any) => any {
+    return this._callback;
+  }
+
+  public set callback(value: (message: any) => void) {
+    this._callback = value;
+  }
 }
