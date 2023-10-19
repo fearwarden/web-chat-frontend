62084db1e5de6d5dc7e97d6268b5bc489d9c7c75
Boris Antonijev <81489899+fearwarden@users.noreply.github.com>
2023-10-19 23:19:17 +0200

    login visual

--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,10 +1,45 @@
+import { Link } from "react-router-dom"
+import Router from "../../routers/Router.tsx";
 
 function LoginPage() {
 
+    async function handleSignIn() {}
+
     return (
-        <>
-            <div>LoginPage</div>
-        </>
+            <div className="bg-gray-900">
+                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                    <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-white">
+                        <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
+                            Webchat Application
+                    </a>
+                    <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">
+                                Sign in to your account
+                            </h1>
+                            <form className="space-y-4 md:space-y-6" onSubmit={handleSignIn} noValidate>
+                                <div>
+                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">Your email</label>
+                                    <input type="email" name="email" id="email" className="border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="name@company.com" required />
+                                </div>
+                                <div>
+                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-white">Password</label>
+                                    <input type="password" name="password" id="password" placeholder="••••••••" className="border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" required />
+                                </div>
+                                <div className="flex items-center justify-between">
+                                    <div className="flex items-start">
+                                        <a href="#" className="text-sm font-medium hover:underline text-blue-500">Forgot password?</a>
+                                    </div>
+                                </div>
+                                <button type="submit" className="w-full text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-primary-800">Sign in</button>
+                                <p className="text-sm font-light text-gray-400">
+                                    Don’t have an account yet? <Link to={Router.REGISTER} className="font-medium hover:underline text-blue-500">Sign up</Link>
+                                </p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
     );
 }
 
