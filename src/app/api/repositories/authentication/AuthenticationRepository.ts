6dc02665d8c93baa1f49c31c87747e740c9be4de
Boris Antonijev <81489899+fearwarden@users.noreply.github.com>
2023-10-25 17:27:07 +0200

    Update AuthenticationRepository.ts

--- src/app/api/repositories/authentication/AuthenticationRepository.ts
+++ src/app/api/repositories/authentication/AuthenticationRepository.ts
@@ -2,10 +2,10 @@ import {HttpClient} from "@/app/api/http/HttpClient.ts";
 
 export class AuthenticationRepository extends HttpClient {
     private collection: string = "/api/v1/auth"
+    protected axiosInstance = this.createInstance();
 
     public async login(email: string, password: string) {
-        const instance = this.createInstance();
-        const result = await instance.post(`${this.collection}/login`, { email, password });
+        const result = await this.axiosInstance.post(`${this.collection}/login`, { email, password });
         return result.data;
     }
 }
