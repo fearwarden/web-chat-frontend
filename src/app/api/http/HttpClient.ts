18ec685c2157a95212cb87b7cc07e628689ffc1a
Boris Antonijev <81489899+fearwarden@users.noreply.github.com>
2023-12-26 00:04:10 +0100

    some fixes

--- src/app/api/http/HttpClient.ts
+++ src/app/api/http/HttpClient.ts
@@ -1,46 +1,49 @@
 import axios, { AxiosInstance, AxiosResponse, AxiosError } from "axios";
 
 export abstract class HttpClient {
-    protected instance: AxiosInstance | undefined;
+  protected instance: AxiosInstance | undefined;
 
-    protected createInstance(): AxiosInstance {
-        this.instance = axios.create({
-            headers: {
-                "Content-Type": "application/json"
-            },
-            withCredentials: true
-        });
-        //this.initializeRequestInterceptor(); add if necessary
-        this.initializeResponseInterceptor();
-        return this.instance;
+  protected createInstance(): AxiosInstance {
+    this.instance = axios.create({
+      headers: {
+        "Content-Type": "application/json",
+      },
+      withCredentials: true,
+    });
+    //this.initializeRequestInterceptor(); add if necessary
+    this.initializeResponseInterceptor();
+    return this.instance;
+  }
+
+  /*
+   * Response interceptor
+   */
+  private initializeResponseInterceptor = () => {
+    this.instance?.interceptors.response.use(
+      this.handleResponse,
+      this.handleError
+    );
+  };
+
+  private handleResponse = ({ data }: AxiosResponse) => data;
+  private handleError = async (error: AxiosError) => {
+    if (error.response?.status === 406) {
+      const originalRequest = error.config;
+      try {
+        await this.refreshToken();
+        return this.instance!(originalRequest!);
+      } catch (e) {
+        return Promise.reject(e);
+      }
     }
+    return Promise.reject(error);
+  };
 
-    /*
-    * Response interceptor
-     */
-    private initializeResponseInterceptor = () => {
-        this.instance?.interceptors.response.use(this.handleResponse, this.handleError);
+  private async refreshToken() {
+    try {
+      await this.instance?.post("/api/v1/auth/refresh");
+    } catch (e) {
+      console.log(e);
     }
-
-    private handleResponse = ({ data }: AxiosResponse) => data;
-    private handleError = async (error: AxiosError) => {
-        if (error.response?.status === 406) {
-            const originalRequest = error.config;
-            try {
-                await this.refreshToken();
-                return this.instance!(originalRequest!);
-            } catch (e) {
-                return Promise.reject(e);
-            }
-        }
-        return Promise.reject(error);
-    };
-
-    private async refreshToken() {
-        try {
-            await this.instance?.post("/api/v1/auth/refresh");
-        } catch (e) {
-            console.log(e);
-        }
-    }
+  }
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,15 @@ import {RouterProvider} from "react-router-dom";
 import Router from "../src/routers/Router.tsx";
 import {Provider} from "react-redux";
 import {store} from "@/store/store.ts";
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "@/dev";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
-        <RouterProvider router={Router.router} />
+        <DevSupport ComponentPreviews={ComponentPreviews}
+                    useInitialHook={useInitial}
+        >
+            <RouterProvider router={Router.router}/>
+        </DevSupport>
     </Provider>
 )
